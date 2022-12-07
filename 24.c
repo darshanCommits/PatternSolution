1e3efae6bb180a5bc49b54767c0dd66e25764edb
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2022-12-07 21:16:47 +0530

    que 24 fixed

--- 24.c
+++ 24.c
@@ -2,9 +2,10 @@
 
 void main()
 {
-    for (int i = 1; i <= 5; i++)
+    for (int i = 5; i >= 1; i--)
     {
-        for(int j = 5; j >= i ; j--) {
+        for (int j = i; j >= 1; j--)
+        {
             printf("%d", j);
         }
         printf("\n");
